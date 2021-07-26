df6fc2908b12f5a7b5316a84ef948e4c3bff345e
tanseeffahad <tanseef_fahad2010@yahoo.com>
2021-07-26 21:38:34 +0500

    Delete FrontEnd/Example directory

--- FrontEnd/Example/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  UsersAPIURL: "https://localhost:44348/",
-  AuthToken: "UserModel",
-};
--- FrontEnd/Example/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-  production: false,
-  UsersAPIURL: "https://localhost:44348/",
-  AuthToken: "UserModel",
-};
